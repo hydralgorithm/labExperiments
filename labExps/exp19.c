4a4fb4093bb48d31672eba46eadeac6bc0ef9784
hydralgorithm <hydralgorithm@gmail.com>
2025-10-22 15:40:27 +0530

    Fixed error

--- labExps/exp19.c
+++ labExps/exp19.c
@@ -1,19 +1,27 @@
 #include <stdio.h>
+#include <math.h>
 int main(){
-    int n;
-    printf("Enter number of terms:\n");
-    scanf("%d", &n);
-    printf("sin(x) = ");
-    for(int i = 0; i < n; i++){
-        int power = 2 * i + 1;
-        if(i > 0){
-            if(i % 2 == 0){
-                printf(" + ");
-            } else {
-                printf(" - ");
+    int n,i,j,x,power,sym=1;
+    double fx,sum=0.0;
+    scanf("%d",&n);
+    scanf("%d",&x);
+    for(i=0;i<n;i++){
+        power=2*i+1;
+        if(i>0){
+            if(i%2==0){
+                sym=1;
+            }
+            else{
+                sym=-1;
             }
         }
-        printf("x^%d/%d!", power, power);
+        double fact = 1.0;
+        for(j=2;j<=power;j++){
+            fact=fact*j;
+        }
+        fx=(sym*pow((double)x,power))/fact;
+        sum+=fx;
     }
+    printf("sin(%d) = %.3f",x,sum);
     return 0;
 }
